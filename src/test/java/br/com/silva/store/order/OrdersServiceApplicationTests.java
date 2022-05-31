1cfa479b228a6e556798eacdf278d67f83e53f04
JEFERSON FERREIRA DA SILVA <jeferson.ferreira.ca@gmail.com>
2022-05-31 15:43:19 -0300

    first commit

---
+++ src/test/java/br/com/silva/store/order/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.silva.store.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
